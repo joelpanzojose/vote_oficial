9f9a780c6a24f42f84ea0f444858b00d39c0702b
Joel José <joelpanzojose835@gmail.com>
2025-10-04 08:52:21 -0300

    Ajustei a tabela da enquete e descrição

--- src/componentes/EnqueteCard/Enquete.jsx
+++ src/componentes/EnqueteCard/Enquete.jsx
@@ -4,20 +4,35 @@ import './EnqueteCard.css'
 function Enquete(props) {
     return (
         <div className="corpoEnquete">
-            <h1>{props.titulo}</h1>
+           
             <div className="CardEnquete">
+            <h1>{props.titulo}</h1>
                 <form>
+                    <div className="enquetecontent">
+
                     {props.opcoes && props.opcoes.map((opcao, idx) => (
                         <div className="divOption" key={idx}>
                             <label htmlFor={`opcao-${idx}`}>{idx + 1}</label>
                             <input type="text" id={`opcao-${idx}`} placeholder={opcao} disabled />
                         </div>
                     ))}
-                    <button>Votar</button>
+
+                    </div>
+                   <div className="divbtnVotar">
+                        <button>Votar</button>
+                   </div>
+                   
                 </form>
             </div>
-            <div className="CardDescricao"></div>
+
+            <div className="CardDescricao">
+                <h2>Descrição</h2>
+                    <p>{props.primeirotexto}</p>
+                    <p>{props.segundotexto}</p>
+            </div>
+          
         </div>
+        
     );
 }
 
