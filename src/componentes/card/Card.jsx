af49c9f979cf904746df0016cee4aaa4efd9579d
Joel José <joelpanzojose835@gmail.com>
2025-10-06 15:45:08 -0300

    Adicionei o conteúdo na aba das estatísticas

--- src/componentes/card/Card.jsx
+++ src/componentes/card/Card.jsx
@@ -1,16 +1,27 @@
 
 import React from 'react';
 import './card.css'
+import { Plus, IconEnquete, IconEstatisticas, Logout, TotalEnquete } from './svgs/mysvg';
+import { Participacaomedia } from './svgs/mysvg';
 
 
 function Card(props) {
 
   return (
-    <div className="card">
-      <h2>{props.titulo}</h2>
-      <p>{props.conteudo}</p>
-      <button>Ver Mais</button>
+    <div className="containerCard">
+     
+      <div className="card">
+          <div className="cardConteudo">
+            <div className={`icon_Cards ${props.bgClass}`}>{props.IconCard_estatisticas}</div>
+
+            <div className="info_card">
+              <div className="titulo"><p>{props.titulo}</p></div>
+              <div className="dados"> <h3>{props.totalvotos}</h3></div>  
+            </div>
+          </div>
+      </div>
     </div>
+
   );
 }
 
--- src/pages/Estatisticas/Estatisticas.jsx
+++ src/pages/Estatisticas/Estatisticas.jsx
@@ -1,15 +1,48 @@
 import './Estatisticas.css'
 import { Link } from 'react-router-dom';
+import Card from '../../componentes/card/Card';
+import { Participacaomedia, EstatisticasCard, Man, Woman } from '../../componentes/card/svgs/mysvg';
 function Estatisticas() {
   return (
     <div className="estatisticas">
       <h1>Estatísticas</h1>
-      <p>Aqui você pode ver as estatísticas das enquetes criadas.</p>
-      {/* Exibição das estatísticas */}
-   
-        <Link to="/">
-          <button className="btn_voltar">Voltar para Home</button>
-        </Link>
+      
+      <div className="divCards-Estatisticas">
+                <Card
+                  IconCard_estatisticas={<Participacaomedia />}
+                  titulo="Total de Votos"
+                  totalvotos="95"
+                  bgClass="bg-azul"
+                />
+
+                <Card
+                  IconCard_estatisticas={<EstatisticasCard />}
+                  titulo="Participação Média"
+                  totalvotos="72%"
+                  bgClass="bg-laranja"
+                />
+
+                <Card
+                  IconCard_estatisticas={<Man />}
+                  titulo="Votos Masculinos"
+                  totalvotos="60"
+                  bgClass="bg-verde"
+                />
+
+                <Card
+                  IconCard_estatisticas={<Woman />}
+                  titulo="Votos Femininos"
+                  totalvotos="35"
+                  bgClass="gb-roxo"
+                />
+      </div>
+               
+
+
+
+      <Link to="/">
+        <button className="btn_voltar">Voltar para Home</button>
+      </Link>
     </div>
   )
 }
